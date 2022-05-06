6bce1f9c3e7d180ac203d66428cf8e9d6b269bca
Murali07 <muraliselvaraj97@gmail.com>
2022-05-06 13:07:40 +0530

    Simple like and dislike counter website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+// Component definition/declaration
+// App - component
+export default function App() {
+  const users = [
+    {
+      name: "Vaibhav",
+      pic:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+      name: "Tejaswini",
+      pic:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHBlcnNvbnxlbnwwfHwwfHw%3D&w=1000&q=80"
+    },
+    {
+      name: "Shreyas",
+      pic:
+        "https://www.opticalexpress.co.uk/media/1064/man-with-glasses-smiling-looking-into-distance.jpg"
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((usr) => (
+        <Msg name={usr.name} pic={usr.pic} />
+      ))}
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [like, setLike] = useState(0);
+  const [dislike, setDisLike] = useState(0);
+  return (
+    <div>
+      <button className="btn-like" onClick={() => setLike(like + 1)}>
+        👍 {like}
+      </button>
+      <button className="btn-dislike" onClick={() => setDisLike(dislike + 1)}>
+        👎 {dislike}
+      </button>
+    </div>
+  );
+}
+
+// props - (like arguments in function)
+
+// JSX  5 - 8 | JavaScript XML
+// JSX -> Webpack + Babel ->  JS
+
+// template syntax - {}
+// Component Msg - Component definition/declaration
+// 1. first letter must be capital
+// 2. Return only one JSX element
+function Msg({ name, pic }) {
+  return (
+    <div className="msg-container">
+      <img className="profile-pic" src={pic} alt={name} />
+      <h1>Hello {name} 👋😎🎉🎉</h1>
+      <Counter />
+    </div>
+  );
+}
